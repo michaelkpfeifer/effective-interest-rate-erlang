03da812cd22ceb6424b3fd41ad3d824e1b605e99
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2017-01-10 23:54:17 +0100

    Add module for computing effective interest rates along with tests.

--- src/effective_interest_rate.erl
+++ src/effective_interest_rate.erl
@@ -1,2 +1,113 @@
 -module(effective_interest_rate).
--compile(export_all).
+-export([effective_interest_rate/1]).
+
+-ifdef(TEST).
+-export([convert_to_payments_with_offset/1]).
+-export([derive_payment_with_offset/1]).
+-export([evaluate/2]).
+-export([first_payment_with_date/1]).
+-export([payment_offset/3]).
+-endif.
+
+-define(MAX_ITERATIONS_COUNT, 16).
+-define(MAX_DIFF_ITERATIONS, 10.0e-10).
+
+effective_interest_rate(PaymentsWithDate) ->
+    PaymentsWithOffset = convert_to_payments_with_offset(PaymentsWithDate),
+    PaymentsWithOffsetDerivative = lists:map(fun(X) -> derive_payment_with_offset(X) end, PaymentsWithOffset),
+    effective_interest_rate(PaymentsWithOffset, PaymentsWithOffsetDerivative, 0, 0.0).
+
+effective_interest_rate(_, _, ?MAX_ITERATIONS_COUNT, Iteration) ->
+    Iteration;
+effective_interest_rate(PaymentsWithOffset,
+                        PaymentsWithOffsetDerivative,
+                        IterationsCount,
+                        PreviousIteration) ->
+    Nominator = evaluate(PaymentsWithOffset, PreviousIteration),
+    Denominator = evaluate(PaymentsWithOffsetDerivative, PreviousIteration),
+    NextIteration = PreviousIteration - (Nominator / Denominator),
+    case abs(NextIteration - PreviousIteration) < ?MAX_DIFF_ITERATIONS of
+        true ->
+            NextIteration;
+        false ->
+            effective_interest_rate(PaymentsWithOffset,
+                                    PaymentsWithOffsetDerivative,
+                                    IterationsCount + 1,
+                                    NextIteration)
+    end.
+
+
+evaluate(Terms, X) ->
+    lists:foldl(fun(Term, Sum) ->
+                        {Amount, Offset} = Term,
+                        Sum + Amount * math:pow((1 + X), -Offset)
+                end,
+                0,
+                Terms).
+
+
+convert_to_payments_with_offset(PaymentsWithDate) ->
+    {_, FirstPaymentDate} = first_payment_with_date(PaymentsWithDate),
+    FirstPaymentOffset = payment_offset_in_year(FirstPaymentDate),
+    {FirstPaymentYear, _, _} = FirstPaymentDate,
+    lists:map(fun(X) -> convert_to_payment_with_offset(X, FirstPaymentYear, FirstPaymentOffset) end, PaymentsWithDate).
+
+convert_to_payment_with_offset(PaymentWithDate, FirstPaymentYear, FirstPaymentOffset) ->
+    {Payment, _} = PaymentWithDate,
+    Offset = payment_offset(PaymentWithDate, FirstPaymentYear, FirstPaymentOffset),
+    {Payment, Offset}.
+
+
+first_payment_with_date([PaymentWithDate | RemainingPaymentsWithDate]) ->
+    first_payment_with_date(PaymentWithDate, RemainingPaymentsWithDate).
+
+first_payment_with_date(PaymentWithDate, []) ->
+    PaymentWithDate;
+first_payment_with_date(PaymentWithDate, [NextPaymentWithDate | NextRemainingPaymentsWithDate]) ->
+    {_, Date} = PaymentWithDate,
+    {_, NextDate} = NextPaymentWithDate,
+    case Date < NextDate of
+        true ->
+            first_payment_with_date(PaymentWithDate, NextRemainingPaymentsWithDate);
+        false ->
+            first_payment_with_date(NextPaymentWithDate, NextRemainingPaymentsWithDate)
+    end.
+
+
+payment_offset_in_year(PaymentDate) ->
+    {Year, _, _} = PaymentDate,
+    GregorianDaysJanFirst = calendar:date_to_gregorian_days({Year, 1, 1}),
+    GregorianDaysNow = calendar:date_to_gregorian_days(PaymentDate),
+    DaysInYear = GregorianDaysNow - GregorianDaysJanFirst,
+
+    case calendar:is_leap_year(Year) of
+        true ->
+            DaysInYear / 366;
+        false ->
+            DaysInYear / 365
+    end.
+
+
+payment_offset(PaymentWithDate, FirstPaymentYear, FirstPaymentOffset) ->
+    {_, PaymentDate} = PaymentWithDate,
+    {Year, _, _} = PaymentDate,
+    YearDifference = Year - FirstPaymentYear,
+    PaymentOffsetInYear = payment_offset_in_year(PaymentDate),
+    PaymentOffsetInYear + YearDifference - FirstPaymentOffset.
+
+
+%% PaymentsWithOffset is a list
+%%   [{A_1, t_1}, ... {A_n, t_n}]
+%% of payments A_k with offsets t_k (in years). PaymentsWithOffset
+%% defines a function
+%%   S(X) = \sum_{k = 1}^n A_n * (1 + X)^{-t_k}.
+%% The derivative of S is
+%%   S'(X) = \sum_{k = 1}^n A_n * (-t_k) * (1 + X)^{-t_k - 1}.
+%% The derivative has the same structure as the original function, so
+%% we can store it in the same way. We simply represent the derivative
+%% as
+%%   [{-t_1 * A_1, -t_1 - 1}, ..., {-t_k * A_k, -t_k - 1}].
+
+derive_payment_with_offset(PaymentWithOffset) ->
+    {Amount, Offset} = PaymentWithOffset,
+    {-Offset * Amount, -(-Offset - 1)}.
--- test/effective_interest_rate_tests.erl
+++ test/effective_interest_rate_tests.erl
@@ -1,2 +1,91 @@
 -module(effective_interest_rate_tests).
 -include_lib("eunit/include/eunit.hrl").
+
+very_simple_effective_interest_rate_test() ->
+    PaymentsWithDate = [{2000, {2013, 6, 1}},
+                        {-1000, {2014, 6, 1}},
+                        {-1000, {2015, 6, 1}}],
+    EffectiveInterestRate = effective_interest_rate:effective_interest_rate(PaymentsWithDate),
+    ?assert(abs(EffectiveInterestRate) < 10.0e-6).
+
+simple_effective_interest_rate_test() ->
+    PaymentsWithDate = [{2000, {2013, 6, 1}},
+                        {-1000, {2014, 6, 1}},
+                        {-1000, {2015, 6, 1}},
+                        {-100, {2015, 7, 1}}],
+    EffectiveInterestRate = effective_interest_rate:effective_interest_rate(PaymentsWithDate),
+    ?assert(EffectiveInterestRate > 0).
+
+effective_interest_rate_for_monthly_payment_test() ->
+    PaymentsWithDate = [{240000, {2015, 1, 1}}] ++
+        [{-1200, {Year, Month, 1}} || Year <- lists:seq(2015, 2034),
+                                      Month <- lists:seq(1, 12)],
+    EffectiveInterestRate = effective_interest_rate:effective_interest_rate(PaymentsWithDate),
+    ?assert(abs(EffectiveInterestRate - 1.91 / 100) < 10.0e-4).
+
+trivial_convert_to_payments_with_offset_test() ->
+    PaymentsWithDate = [{2000, {2015, 6, 1}}],
+    PaymentsWithOffset = effective_interest_rate:convert_to_payments_with_offset(PaymentsWithDate),
+    [{Payment, Offset} | _] = PaymentsWithOffset,
+    ?assertEqual(2000, Payment),
+    ?assertEqual(0.0, Offset).
+
+simple_convert_to_payments_with_offset_test() ->
+    PaymentsWithDate = [{2000, {2013, 6, 1}},
+                        {-1000, {2014, 6, 1}},
+                        {-1000, {2015, 6, 1}},
+                        {-100, {2015, 7, 1}}],
+    PaymentsWithOffset = effective_interest_rate:convert_to_payments_with_offset(PaymentsWithDate),
+    Payments = lists:map(fun(X) -> element(1, X) end, PaymentsWithOffset),
+    [Offset1, Offset2, Offset3, Offset4] = lists:map(fun(X) -> element(2, X) end, PaymentsWithOffset),
+
+    ?assertEqual([2000, -1000, -1000, -100], Payments),
+    ?assertEqual(0.0, Offset1),
+    ?assertEqual(1.0, Offset2),
+    ?assertEqual(2.0, Offset3),
+    ?assert(Offset4 > 2.05),
+    ?assert(Offset4 < 2.1).
+
+
+trivial_first_payment_with_date_test() ->
+    PaymentsWithDate = [{2000, {2015, 6, 1}}],
+    FirstPaymentWithDate = effective_interest_rate:first_payment_with_date(PaymentsWithDate),
+    ?assertEqual({2000, {2015, 6, 1}}, FirstPaymentWithDate).
+
+simple_first_payment_with_date_test() ->
+    PaymentsWithDate = [{-1000, {2015, 6, 1}},
+                        {-1000, {2014, 6, 1}},
+                        {2000, {2013, 6, 1}},
+                        {-100, {2015, 7, 1}}],
+    FirstPaymentWithDate = effective_interest_rate:first_payment_with_date(PaymentsWithDate),
+    ?assertEqual({2000, {2013, 6, 1}}, FirstPaymentWithDate).
+
+
+payment_offset_test() ->
+    PaymentWithDate1 = {1000, {2015, 1, 1}},
+    PaymentOffset1 = effective_interest_rate:payment_offset(PaymentWithDate1, 2015, 0),
+    ?assertEqual(0.0, PaymentOffset1),
+    PaymentWithDate2 = {1000, {2016, 1, 1}},
+    PaymentOffset2 = effective_interest_rate:payment_offset(PaymentWithDate2, 2015, 0),
+    ?assertEqual(1.0, PaymentOffset2),
+    PaymentWithDate3 = {1000, {2016, 7, 2}},
+    PaymentOffset3 = effective_interest_rate:payment_offset(PaymentWithDate3, 2016, 0),
+    ?assertEqual(0.5, PaymentOffset3).
+
+
+derive_payment_with_offset_test() ->
+    ?assertEqual({0.0, 1.0}, effective_interest_rate:derive_payment_with_offset({1000.0, 0.0})),
+    ?assertEqual({-1000.0, 2.0}, effective_interest_rate:derive_payment_with_offset({1000.0, 1.0})),
+    ?assertEqual({-2000.0, 3.0}, effective_interest_rate:derive_payment_with_offset({1000.0, 2.0})),
+    ?assertEqual({-500.0, 1.5}, effective_interest_rate:derive_payment_with_offset({1000.0, 0.5})).
+
+
+evaluate_test() ->
+    PaymentsWithDate = [{2000, {2013, 6, 1}},
+                        {-1000, {2014, 6, 1}},
+                        {-1000, {2015, 6, 1}}],
+    PaymentsWithOffset = effective_interest_rate:convert_to_payments_with_offset(PaymentsWithDate),
+    Value0 = effective_interest_rate:evaluate(PaymentsWithOffset, 0.0),
+    ?assertEqual(0.0, Value0),
+    Value1 = effective_interest_rate:evaluate(PaymentsWithOffset, 1.0),
+    ?assertEqual(1250.0, Value1).
