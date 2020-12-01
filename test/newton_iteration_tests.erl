3843d39264358cd8c4a0536b337632c7ed02e71b
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2020-12-01 18:15:15 +0100

    Use absolute value to simplify assertions

--- test/newton_iteration_tests.erl
+++ test/newton_iteration_tests.erl
@@ -7,8 +7,8 @@ iterate_finds_the_root_of_the_identity_function_test() ->
 
     {ok, Root} = newton_iteration:iterate(F, Fp, 1.0, 1.0e-9, 4),
 
-    ?assert(Root < 1.0e-9),
-    ?assert(Root > -1.0e-9).
+    ?assert(abs(Root) < 1.0e-9).
+
 
 iterate_does_not_find_root_of_polynomial_if_too_few_iterations_are_allowed_test() ->
     F = fun(X) -> math:pow(X, 4) - 1 end,
@@ -16,11 +16,11 @@ iterate_does_not_find_root_of_polynomial_if_too_few_iterations_are_allowed_test(
 
     ?assertEqual(error, newton_iteration:iterate(F, Fp, 2.0, 1.0e-9, 4)).
 
+
 iterate_finds_root_of_polynomial_if_sufficient_iterations_are_allowed_test() ->
     F = fun(X) -> math:pow(X, 4) - 1 end,
     Fp = fun(X) -> 4 * math:pow(X, 3) end,
 
     {ok, Root} = newton_iteration:iterate(F, Fp, 2.0, 1.0e-9, 8),
 
-    ?assert(Root < 1.0 + 1.0e-9),
-    ?assert(Root > 1.0 - 1.0e-9).
+    ?assert(abs(Root - 1.0) < 1.0e-9).
--- test/payment_stream_tests.erl
+++ test/payment_stream_tests.erl
@@ -7,6 +7,7 @@ erliest_payment_of_a_stream_of_one_payments_is_the_given_payment_test() ->
 
     ?assertEqual(payment_stream:earliest_payment(PaymentStream), Payment).
 
+
 earliest_payment_of_a_stream_of_payments_is_the_earliest_one_test() ->
     EarliestPayment = {-1000, {2020, 1, 1}},
     Payment = {500, {2020, 7, 1}},
@@ -14,6 +15,7 @@ earliest_payment_of_a_stream_of_payments_is_the_earliest_one_test() ->
     PaymentStream = [NewestPayment, Payment, EarliestPayment],
 
     ?assertEqual(payment_stream:earliest_payment(PaymentStream), EarliestPayment).
+
 
 relative_date_for_relative_payment_stream_with_one_payment_is_zero_test() ->
     PaymentStream = [{1000, {2020, 1, 1}}],
@@ -33,6 +35,7 @@ relative_date_for_earliest_payment_in_relative_payment_stream_is_zero_test() ->
     ?assertEqual(payment_stream:to_relative_payment_stream(PaymentStream),
                  [{-1000, 0.0}, {500, 1.0}, {500, 2.0}]).
 
+
 relative_date_respects_non_leap_year_in_relative_payment_stream_test() ->
     PaymentJan01 = {-1000, {2019, 1, 1}},
     PaymentJan02 = {500, {2019, 1, 2}},
@@ -41,10 +44,9 @@ relative_date_respects_non_leap_year_in_relative_payment_stream_test() ->
 
     [{_, _}, {_, T_1}, {_, T_2}] = payment_stream:to_relative_payment_stream(PaymentStream),
 
-    ?assert(T_1 > 1 / 365 - 1.0e-9),
-    ?assert(T_1 < 1 / 365 + 1.0e-9),
-    ?assert(T_2 > 364 / 365 - 1.0e-9),
-    ?assert(T_2 < 364 / 365 + 1.0e-9).
+    ?assert(abs(T_1 - 1 / 365) < 1.0e-9),
+    ?assert(abs(T_2 - 364 / 365) < 1.0e-9).
+
 
 relative_date_respects_leap_year_in_relative_payment_stream_test() ->
     PaymentJan01 = {-1000, {2020, 1, 1}},
@@ -54,10 +56,9 @@ relative_date_respects_leap_year_in_relative_payment_stream_test() ->
 
     [{_, _}, {_, T_1}, {_, T_2}] = payment_stream:to_relative_payment_stream(PaymentStream),
 
-    ?assert(T_1 > 1 / 366 - 1.0e-9),
-    ?assert(T_1 < 1 / 366 + 1.0e-9),
-    ?assert(T_2 > 365 / 366 - 1.0e-9),
-    ?assert(T_2 < 365 / 366 + 1.0e-9).
+    ?assert(abs(T_1 - 1 / 366) < 1.0e-9),
+    ?assert(abs(T_2 - 365 / 366) < 1.0e-9).
+
 
 leap_years_are_respected_across_year_boundaries_in_relative_payment_stream_test() ->
     FirstPayment = {-1000, {2019, 12, 1}},
@@ -66,8 +67,8 @@ leap_years_are_respected_across_year_boundaries_in_relative_payment_stream_test(
 
     [{_, _}, {_, T_1}] = payment_stream:to_relative_payment_stream(PaymentStream),
 
-    ?assert(T_1 > 1 - 334 / 365 + 30 / 366 - 1.0e-9),
-    ?assert(T_1 < 1 - 334 / 365 + 30 / 366 + 1.0e-9).
+    ?assert(abs(T_1 - (1 - 334 / 365 + 30 / 366)) < 1.0e-9).
+
 
 net_present_value_for_an_interest_rate_of_0_is_equal_to_the_sum_of_amounts_test() ->
     Payment1 = {-1000, {2019, 1, 1}},
@@ -78,11 +79,10 @@ net_present_value_for_an_interest_rate_of_0_is_equal_to_the_sum_of_amounts_test(
     Npv = payment_stream:net_present_value(
             payment_stream:to_relative_payment_stream([Payment1, Payment2, Payment3, Payment4])
            ),
-
     Npv0 = Npv(0.0),
 
-    ?assert(Npv0 > 200.0 - 1.0e-9),
-    ?assert(Npv0 < 200.0 + 1.0e-9).
+    ?assert(abs(Npv0 - 200.0) < 1.0e-9).
+
 
 net_present_value_returns_the_expected_manually_computed_result_test() ->
     Payment1 = {-1000, {2019, 1, 1}},
@@ -92,11 +92,10 @@ net_present_value_returns_the_expected_manually_computed_result_test() ->
     Npv = payment_stream:net_present_value(
             payment_stream:to_relative_payment_stream([Payment1, Payment2, Payment3])
            ),
-
     Npv1 = Npv(1.0),
 
-    ?assert(Npv1 > -625.0 - 1.0e-9),
-    ?assert(Npv1 < -625.0 + 1.0e-9).
+    ?assert(abs(Npv1 + 625.0) < 1.0e-9).
+
 
 net_present_value_derivative_returns_the_expected_manually_computed_result_test() ->
     Payment1 = {-1000, {2019, 1, 1}},
@@ -106,8 +105,6 @@ net_present_value_derivative_returns_the_expected_manually_computed_result_test(
     Npvp = payment_stream:net_present_value_derivative(
             payment_stream:to_relative_payment_stream([Payment1, Payment2, Payment3])
            ),
-
     Npvp1 = Npvp(1.0),
 
-    ?assert(Npvp1 > -250.0 - 1.0e-9),
-    ?assert(Npvp1 < -205.0 + 1.0e-9).
+    ?assert(abs(Npvp1 + 250.0) < 1.0e-9).
